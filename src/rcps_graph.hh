966458593235fe1dd5e6aa29be2742af1875afca
Martin Hruska <hruskamartin25@gmail.com>
2014-04-29 01:25:21 +0200

    renamed from rcsp to rcps

---
+++ src/rcps_graph.hh
@@ -0,0 +1,39 @@
+#ifndef RCPS_GRAPH_HH
+#define RCPS_GRAPH_HH
+
+#include <vector>
+#include "rcps_instance.hh"
+
+namespace RCPSSolver
+{
+    class PrecedenceGraph;
+    int parseGraphFromRCPSInstance(PrecedenceGraph& graph,
+            const RCPSInstance& instance);
+    int rcpsFloydWarshall(PrecedenceGraph& graph);
+    int storeGraphValuesToInstance(const PrecedenceGraph& graph,
+            RCPSInstance& instance);
+}
+
+/**
+ * Class for representation of a graph base on precedence relation
+ * for a RCPS instance.
+ */
+class RCPSSolver::PrecedenceGraph
+{
+private: // private data type
+    typedef std::vector<int> Row;
+    typedef std::vector<std::vector<int>> Graph;
+private: // private data types
+    Graph graph_;
+    int size_;
+public: // public constructors
+    PrecedenceGraph(int size);
+
+    friend int parseGraphFromRCPSInstance(PrecedenceGraph& graph,
+            const RCPSInstance& instance);
+    friend int rcpsFloydWarshall(PrecedenceGraph& graph);
+    friend int storeGraphValuesToInstance(const PrecedenceGraph& graph,
+            RCPSInstance& instance);
+};
+
+#endif
---
+++ src/rcps_parser.hh
@@ -0,0 +1,25 @@
+#ifndef RCPS_SOLVER_PARSER_HH
+#define RCPS_SOLVER_PARSER_HH
+
+#include "rcps_instance.hh"
+
+#include <string>
+
+namespace RCPSSolver
+{
+    class Parser;
+}
+
+class RCPSSolver::Parser
+{
+public:
+    static int parse(RCPSInstance& instance, const char* source);
+    static void toString(RCPSInstance& instance);
+private:
+    static int getValue(std::string& line);
+    static int parsePSPInfo(RCPSInstance& instance, std::string& line);
+    static int parsePrecedenceLine(RCPSInstance& instance, std::string& line);
+    static int parseDemandLine(RCPSInstance& instance, std::string& line);
+    static int parseResourcesLine(RCPSInstance& instance, std::string& line);
+};
+#endif
