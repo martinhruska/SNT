cf744e0a80e8ec77e8eb519622db70dd0c319912
Martin Hruska <hruskamartin25@gmail.com>
2014-05-01 16:50:15 +0200

    asdfasf

---
+++ src/sat_solver/glucose/simp/depend.mk
@@ -0,0 +1,37 @@
+/home/martin/compiled/glucose/glucose-3.0/simp/Main.o /home/martin/compiled/glucose/glucose-3.0/simp/Main.or /home/martin/compiled/glucose/glucose-3.0/simp/Main.od /home/martin/compiled/glucose/glucose-3.0/simp/Main.op: /home/martin/compiled/glucose/glucose-3.0/simp/Main.cc \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../utils/System.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/IntTypes.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../utils/ParseUtils.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../utils/Options.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Vec.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/XAlloc.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/Dimacs.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/SolverTypes.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Alg.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Map.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Alloc.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../simp/SimpSolver.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Queue.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/Solver.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Heap.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/BoundedQueue.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/Constants.h
+/home/martin/compiled/glucose/glucose-3.0/simp/SimpSolver.o /home/martin/compiled/glucose/glucose-3.0/simp/SimpSolver.or /home/martin/compiled/glucose/glucose-3.0/simp/SimpSolver.od /home/martin/compiled/glucose/glucose-3.0/simp/SimpSolver.op: \
+ /home/martin/compiled/glucose/glucose-3.0/simp/SimpSolver.cc \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Sort.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Vec.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/IntTypes.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/XAlloc.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../simp/SimpSolver.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Queue.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/Solver.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Heap.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Alg.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../utils/Options.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../utils/ParseUtils.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/SolverTypes.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Map.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../mtl/Alloc.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/BoundedQueue.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../core/Constants.h \
+ /home/martin/compiled/glucose/glucose-3.0/simp/../utils/System.h
