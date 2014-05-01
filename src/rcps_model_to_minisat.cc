879497eb795ed40ec07697a42163b13c2bf5f2f7
Martin Hruska <hruskamartin25@gmail.com>
2014-05-02 01:15:54 +0200

    another fail

---
+++ src/rcps_model_to_minisat.cc
@@ -0,0 +1,55 @@
+#include "rcps_model_to_minisat.hh"
+
+#include "minisat/mtl/Vec.h"
+
+#include <iostream>
+
+using namespace RCPSSolver;
+
+/**
+ * Transforms created sat model to a SAT model in SAT solver
+ * @param[in] model Internal SAT model of RCSP sovler
+ * @param[out] solver SAT solver
+ */
+int RCPSModel2Minisat::transformModel2Solver(
+        RCPSSATModel& model, Minisat::Solver& solver)
+{
+    int res1 = transformClauseDb2Solver(model.getConsistency(), solver);
+    int res2 = transformClauseDb2Solver(model.getPrecedence(), solver);
+    int res3 = transformClauseDb2Solver(model.getStart(), solver);
+    int res4 = transformClauseDb2Solver(model.getTimeCons(), solver);
+
+    if (res1 != 0 || res2 != 0 || res3 != 0 || res4 != 0)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+    
+int RCPSModel2Minisat::transformClauseDb2Solver(ClauseDb& clauseDb,
+        Minisat::Solver& solver)
+{
+    for (Clause& cl : clauseDb)
+    {
+        Minisat::vec<Minisat::Lit> gClause;
+        for (Literal& l : cl)
+        {
+            Minisat::Var var;
+            if (createdVars_.count(l.var->id))
+            {
+                var = createdVars_[l.var->id];
+            }
+            else
+            {
+                var = solver.newVar(Minisat::lbool(true),true);
+                createdVars_.insert(std::make_pair(l.var->id, var));
+                createdVarsReverse_.insert(std::make_pair(var,l.var->id));
+            }
+            gClause.push(Minisat::mkLit(var, !l.sign));
+        }
+        solver.addClause(gClause);
+    }
+
+    return 0;
+}
---
+++ src/rcps_model_to_minisat.hh
@@ -0,0 +1,34 @@
+#ifndef RCPS_MODEL_MINISAT
+#define RCPS_MODEL_MINISAT
+
+#include "rcps_sat_model.hh"
+#include "minisat/core/Solver.h"
+
+#include <unordered_map>
+
+namespace RCPSSolver
+{
+    class RCPSModel2Minisat;
+}
+
+class RCPSSolver::RCPSModel2Minisat
+{
+private: // data types
+    typedef std::unordered_map<Minisat::Var,int> ReverseVarMap;
+    typedef std::unordered_map<int,Minisat::Var> VarMap;
+private: //data members
+    VarMap createdVars_;
+    ReverseVarMap createdVarsReverse_;
+public: // public constructors
+    RCPSModel2Minisat() : createdVars_(), createdVarsReverse_()
+    {}
+public: // public functions
+    int transformModel2Solver(RCPSSATModel& model, Minisat::Solver& solver);
+    ReverseVarMap& getReverseVar() {return createdVarsReverse_;}
+    VarMap& getVarMap() {return createdVars_;}
+
+private: // private function
+    int transformClauseDb2Solver(ClauseDb& clauseDb, Minisat::Solver& solver);
+};
+
+#endif
