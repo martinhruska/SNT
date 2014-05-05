edf508a5eeeb74bf1816ff2131d15c1ea70308c5
Martin Hruska <hruskamartin25@gmail.com>
2014-05-05 11:53:34 +0200

    removed binary

--- src/sat_solver/glucose/core/depend.mk
+++ src/sat_solver/glucose/core/depend.mk
@@ -13,11 +13,7 @@
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/Solver.h \
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../mtl/Heap.h \
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/BoundedQueue.h \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/Constants.h \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/../../../rcps_sat_model.hh \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/../../../rcps_solver_types.hh \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/../../../rcps_instance.hh \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/../../../rcps_instance.hh
+ /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/Constants.h
 /home/martin/projects/SNT/src/sat_solver/glucose/core/Solver.o /home/martin/projects/SNT/src/sat_solver/glucose/core/Solver.or /home/martin/projects/SNT/src/sat_solver/glucose/core/Solver.od /home/martin/projects/SNT/src/sat_solver/glucose/core/Solver.op: /home/martin/projects/SNT/src/sat_solver/glucose/core/Solver.cc \
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../mtl/Sort.h \
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../mtl/Vec.h \
@@ -33,10 +29,10 @@
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../mtl/Alloc.h \
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/BoundedQueue.h \
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/Constants.h \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/../../../rcps_sat_model.hh \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/../../../rcps_solver_types.hh \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/../../../rcps_instance.hh \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../core/../../../rcps_instance.hh \
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../utils/System.h \
  /home/martin/projects/SNT/src/sat_solver/glucose/core/../../../rcps_model_to_glucose.hh \
- /home/martin/projects/SNT/src/sat_solver/glucose/core/../../../rcps_sat_model.hh
+ /home/martin/projects/SNT/src/sat_solver/glucose/core/../../../rcps_sat_model.hh \
+ /home/martin/projects/SNT/src/sat_solver/glucose/core/../../../rcps_solver_types.hh \
+ /home/martin/projects/SNT/src/sat_solver/glucose/core/../../../rcps_instance.hh \
+ /home/martin/projects/SNT/src/sat_solver/glucose/core/../../../rcps_sat_model.hh \
+ /home/martin/projects/SNT/src/sat_solver/glucose/core/../../../rcps_instance.hh
