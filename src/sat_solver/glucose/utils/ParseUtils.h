2001e772ff3d9779d57c5e0b4b40ffb64e6f0c7d
Martin Hruska <hruskamartin25@gmail.com>
2014-05-10 11:59:42 +0200

    removed lz

--- src/sat_solver/glucose/utils/ParseUtils.h
+++ src/sat_solver/glucose/utils/ParseUtils.h
@@ -25,7 +25,7 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include <stdio.h>
 #include <math.h>
 
-#include <zlib.h>
+//#include <zlib.h>
 
 namespace Glucose {
 
